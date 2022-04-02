2c5a4c93d6a6d41f57bb0f41df5477e4163f3be6
João Walter Amadeu <joaowa2002@live.com>
2022-04-01 23:17:41 -0300

    Update novo-caminhao.component.ts

--- src/app/rastreador/novo-caminhao/novo-caminhao.component.ts
+++ src/app/rastreador/novo-caminhao/novo-caminhao.component.ts
@@ -46,6 +46,7 @@ export class NovoCaminhaoComponent implements OnInit {
         })
         this.fabricante = new Fabricante('');
       this.caminhao = new Caminhao("", "", "", "", "", this.fabricante);
+      this.acao = "Atualizar"
     } else {
       this.banco.object('/caminhao/' + this.caminhao.key).update(this.caminhao)
       this.caminhao = new Caminhao("", "", "", "", "", this.fabricante);
